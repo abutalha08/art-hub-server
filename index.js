13d978af542afa0240fbb8c97d8c63d53c1000ba
abutalha08 <taufique752@gmail.com>
2026-06-21 04:55:51 +0600

    Make patch api for edit artworks

--- index.js
+++ index.js
@@ -114,6 +114,19 @@ app.get('/api/artworks/:email', async (req, res) => {
   res.json(result);
 });
 
+ app.patch("/api/artworks/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedData = req.body;
+    //   console.log(updatedData);
+
+      const result = await artworksCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData },
+      );
+
+      res.json(result);
+    });
+
 
 
 
